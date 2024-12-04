c6014b0012fb52fe1db3cef0fc8f9af073b12d40
DonSlon <lukindihel@gmail.com>
2024-12-04 10:31:12 +0100

    Day 3 part 1

---
+++ Day3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace Day3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string lines = File.ReadAllText("./input.txt");
+        var match = Regex.Match(lines, "mul\\(\\d{1,3},\\d{1,3}\\)");
+        int sum = 0;
+        while (match.Success)
+        {
+            Match digits = Regex.Match(match.Value, "\\d{1,3}");
+            int digitOne = int.Parse(digits.Value);
+            digits = digits.NextMatch();
+            int digitTwo = int.Parse(digits.Value);
+            sum += digitOne * digitTwo;
+            match = match.NextMatch();
+        }
+        Console.WriteLine($"Total sum is: {sum}");
+    }
+}
