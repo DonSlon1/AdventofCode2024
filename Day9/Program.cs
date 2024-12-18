b9c7dfa8d6310ff7d696c90f1cf1d80f71c3f57f
DonSlon <lukindihel@gmail.com>
2024-12-18 11:56:59 +0100

    Day 9 part 1

---
+++ Day9/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = File.ReadAllLines("input.txt").First();
+            List<int> diskmap = inputLine.Select(c => int.Parse(c.ToString())).ToList();
+
+            List<int> fileblocks = new List<int>();
+            int freespace = 0;
+            int id = 0;
+
+            bool isEmpty = false;
+            foreach (var digit in diskmap)
+            {
+                if (isEmpty)
+                {
+                    for (int j = 0; j < digit; j++)
+                        fileblocks.Add(-1);
+                }
+                else
+                {
+                    for (int j = 0; j < digit; j++)
+                        fileblocks.Add(id);
+                    id++;
+                }
+
+                isEmpty = !isEmpty; // Toggle between filled and empty states
+            }
+
+            int FreeSpaceIndex(int maxIndex)
+            {
+                for (int i = freespace; i < maxIndex; i++)
+                {
+                    if (fileblocks[i] == -1)
+                        return i;
+                }
+                return -1;
+            }
+
+            for (int i = fileblocks.Count - 1; i >= 0; i--)
+            {
+                if (fileblocks[i] >= 0)
+                {
+                    int freespaceIndex = FreeSpaceIndex(i);
+                    if (freespaceIndex < 0)
+                        break; // No available free space found
+
+                    fileblocks[freespaceIndex] = fileblocks[i];
+                    fileblocks[i] = -1;
+
+                    freespace = freespaceIndex + 1;
+                }
+            }
+
+            long checksum = 0;
+            for (int i = 0; i < fileblocks.Count; i++)
+            {
+                if (fileblocks[i] == -1)
+                    break; // Stop at the first free space
+
+                checksum += (long)fileblocks[i] * i;
+            }
+
+            Console.WriteLine($"Checksum: {checksum}");
+        }
+    }
+}
